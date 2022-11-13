49efae133b71775394a04f58b90c2e63cbf029ec
Kevin Orge <kevinorge@gmail.com>
2022-11-13 15:14:49 +0800

    hash password before saving

--- api/neko_users/serializers.py
+++ api/neko_users/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def doPasswordHash(self, password):
+        if password != None and len(password) > 0:
+            return make_password(password)
+        return None
+
+    def prepareDataBeforeSave(self, validated_data):
+        password = validated_data.get('password')
+        updates = {}
+        if password != None and len(password) > 0:
+            updates['password'] = make_password(password)
+        return updates
+
+    def create(self, validated_data):
+        validated_data = {**validate_data, **self.prepareDataBeforeSave(validated_data)}
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        partials = self.prepareDataBeforeSave(validated_data)
+        for prop in partials:
+            setattr(instance, prop, partials[prop])
+        instance.save()
+        return instance
