3020baa9149005a134e64448a5519944e2d5e53b
Kevin Orge <kevinorge@gmail.com>
2022-11-13 12:04:45 +0800

    common module for any apps to access

--- api/helpdesk_api/settings.py
+++ api/helpdesk_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'neko_commons.apps.NekoCommonsConfig',
     'tickets.apps.TicketsConfig',
     'neko_users.apps.NekoUsersConfig',
 ]
