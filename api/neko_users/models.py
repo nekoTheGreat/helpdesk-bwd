bd1abc01b59846ced0b41cddf4615fb1af060a1b
Kevin Orge <kevinorge@gmail.com>
2022-11-13 12:21:10 +0800

    removed username usage in user manager, fixed imports

--- api/neko_users/models.py
+++ api/neko_users/models.py
@@ -1,4 +1,6 @@
+from django.apps import apps
 from django.db import models
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User, AbstractUser, UserManager, PermissionsMixin, UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -6,12 +8,12 @@ from django.utils import timezone
 from neko_commons.models import BaseModel
 
 class NekoUserManager(UserManager):
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
         """
         if not email:
-            raise ValueError("The given username must be set")
+            raise ValueError("The given email must be set")
         email = self.normalize_email(email)
         # Lookup the real model class from the global app registry so this
         # manager method can be used in migrations. This is fine because
@@ -24,12 +26,12 @@ class NekoUserManager(UserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user( email, password, **extra_fields)
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -38,7 +40,7 @@ class NekoUserManager(UserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser, PermissionsMixin, BaseModel):
