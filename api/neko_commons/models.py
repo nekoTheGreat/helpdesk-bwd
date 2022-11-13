4fa187e3ab4f49fdf44443eab4344d28ea2852c6
Kevin Orge <kevinorge@gmail.com>
2022-11-13 21:17:22 +0800

    moved attachment model to commons

--- api/neko_commons/models.py
+++ api/neko_commons/models.py
@@ -6,3 +6,13 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+class Attachment(BaseModel):
+    type = models.CharField(max_length=30, db_index=True)
+    identifier = models.BigIntegerField(db_index=True)
+    disk_type = models.CharField(max_length=30)
+    file_path = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "attachments"
