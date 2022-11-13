f4377e5d7ff00b3b5514496d3e817311b3466f22
Kevin Orge <kevinorge@gmail.com>
2022-11-13 10:43:55 +0800

    customized built in user model and made migrations

--- api/neko_users/models.py
+++ api/neko_users/models.py
@@ -1,3 +1,89 @@
 from django.db import models
 
 
+from django.contrib.auth.models import User, AbstractUser, UserManager, PermissionsMixin, UnicodeUsernameValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+class NekoUserManager(UserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractUser, PermissionsMixin):
+    username = None
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    email_verified = models.BooleanField(
+        _('email_verified'), 
+        default=False,
+        help_text=('Either email is verified or not')
+    )
+    street_address = models.CharField(max_length=255, null=True, blank=True)
+    purok = models.CharField(max_length=255, null=True, blank=True)
+    poblacion = models.CharField(max_length=255, null=True, blank=True)
+    barangay = models.CharField(max_length=255, null=True, blank=True)
+    municipality = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    province = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255)
+
+    objects = NekoUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        db_table = "users"
+
+    @property
+    def token(self):
+        pass
