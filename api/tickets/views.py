c30aaaf0186c0a3ac36a728de77bf96dedaab269
Kevin Orge <kevinorge@gmail.com>
2022-11-14 10:34:35 +0800

    moved uploaded images into attachments directory

--- api/tickets/views.py
+++ api/tickets/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 from .serializers import TicketSerializer
 from .models import Ticket
 from .services import save_ticket
+from uuid import uuid4
+import os
+from django.conf import settings
 
 
 @api_view(['GET', 'POST'])
@@ -36,6 +39,18 @@ def get_ticket(request, id: int):
 def store(request, id: int = None):
     try:
         data = save_ticket(request.data, id)
+        processAttachments(data, request.FILES)        
+
         return Response(data, status=status.HTTP_200_OK)
     except Ticket.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+def processAttachments(data, files):
+    if len(files) == 0:
+        return None
+    images = files.getlist('images')
+    for file in images:
+        path = os.path.join(settings.ATTACHMENTS_DIR, str(uuid4())+".jpg")
+        with open(path, 'wb+') as dest:
+            for chunk in file.chunks():
+                dest.write(chunk)    
