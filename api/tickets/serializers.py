e42d750091a4a53c01603d005ea17b80da77d939
Kevin Orge <kevinorge@gmail.com>
2022-11-16 10:10:20 +0800

    ticket serializer added photos with attachment serializer

--- api/tickets/serializers.py
+++ api/tickets/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Ticket
-
+from neko_commons.models import Attachment
+from neko_commons.serializers import AttachmentSerializer
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+    photos = serializers.SerializerMethodField()
+    def get_photos(self, instance):
+        return AttachmentSerializer(instance.photos, many=True).data
