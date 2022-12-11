93ff6b0f44c1c0318875939a582e509138ea040f
Kevin Orge <kevinorge@gmail.com>
2022-12-11 21:14:25 +0800

    registration page select email, fb or gmail

---
+++ client/src/pages/RegisterPage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { SimpleTicketForm } from 'src/types/ticket';
+
+const form = reactive<SimpleTicketForm>({
+    subject: '', description: '', address: '',
+    contact: '', images: [],
+})
+</script>
+<template>
+    <q-page class="q-pa-sm">
+        <div class="row justify-center">
+            <div class="col-12 col-sm-5 col-md-3 col-lg-3 col-xl-2">
+                <div class="q-px-xl q-mt-lg q-pt-sm q-pb-lg bg-primary">
+                    <form method="POST" @submit.prevent="">
+                        <div class="q-mb-lg">
+                            <label class="text-h6 text-weight-bold text-secondary">Registration</label>
+                        </div>
+                        <q-btn color="grey-1" text-color="black" label="Register using Email" style="width:100%"
+                            class="q-mb-sm" no-caps />
+                        <q-btn color="blue-fb" text-color="secondary" label="Register via Facebook" class="q-mb-sm"
+                            style="width:100%" no-caps />
+                        <q-btn color="white" text-color="red-gmail" label="Register via GMail" class="q-mb-sm"
+                            style="width:100%" no-caps />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </q-page>
+</template>
