807030252e9c150d6b0e6e58484ab41ef15158f4
Kevin Orge <kevinorge@gmail.com>
2022-12-23 19:56:28 +0800

    basic search form

--- client/src/pages/TicketsPage.vue
+++ client/src/pages/TicketsPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 const rows = ref([
     { id: 112312313, subject: 'jey 1 1231 qwdad qe231312', user: 'Kevin Orge', location: 'at manga street, asda ada dasda ass as dasd asdasdsa dsad as' }
@@ -11,10 +11,21 @@ const columns = [
     { name: 'location', field: 'location', label: 'Location', align: 'left' },
     { name: 'actions', field: 'actions', label: '', align: 'center', headerStyle: 'width:70px' },
 ];
-const onItemClick = () => 1
+const form = reactive({
+    term: ''
+});
+
+const onItemClick = () => 1;
+const onSubmitSearch = () => 1;
 </script>
 <template>
     <q-page class="q-pa-sm">
+        <div class="row">
+            <q-input type="text" outlined class="col-12 col-sm-8 col-md-6 col-lg-5 q-mb-sm q-pr-sm bg-grey-1" dense
+                label="Search" v-model="form.term" />
+            <q-btn type="button" color="primary" text-color="secondary" class="q-mb-sm" label="Search"
+                @click="onSubmitSearch" />
+        </div>
         <div class="row">
             <div class="col-12">
                 <q-table :rows="rows" :columns="columns" row-key="id" :pagination="{ rowsPerPage: 50 }"
