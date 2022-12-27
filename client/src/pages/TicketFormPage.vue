9e604e2938b947db6d30689d4ed058b5f52ad69f
Kevin Orge <kevinorge@gmail.com>
2022-12-27 23:13:49 +0800

    added image uploader and updated layout of ticket form

--- client/src/pages/TicketFormPage.vue
+++ client/src/pages/TicketFormPage.vue
@@ -1,8 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { Loading } from 'quasar';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const props = defineProps({
-    id: { type: Number, default: () => null },
+    id: { default: () => null },
 });
 const form = ref({
     title: '',
@@ -12,26 +15,59 @@ const form = ref({
     barangay: '',
     municipality: '',
 });
-const onClickSave = () => 1;
-const onClickCancel = () => 1;
+const imageUploader = ref(null);
+const loading = ref(false);
+watch(loading, () => {
+    if (loading.value) Loading.show();
+    else Loading.hide();
+});
+
+const imageUploading = () => {
+    loading.value = true;
+};
+const imageUploaded = () => {
+    loading.value = false;
+};
+const imageUploadFailed = () => {
+    loading.value = false;
+};
+const onClickSave = () => {
+    imageUploader.value.upload();
+};
+const onClickCancel = () => {
+    let route = { path: '/tickets' };
+    if (props.id) route = { name: 'ticket-view', params: { id: props.id } };
+    router.replace(route);
+};
 </script>
 <template>
     <q-page class="q-pa-sm">
         <div class="row justify-center">
-            <div class="col-12 col-sm-9 col-md-9 col-lg-7 col-xl-6 rounded-borders shadow-10 q-pa-md q-mt-md">
+            <div class="col-12 col-sm-9 col-md-9 col-lg-6 col-xl-4 rounded-borders shadow-10 q-pa-md q-mt-md">
                 <div class="q-mb-lg">
                     <label class="text-h6 text-weight-bold text-primary">Create Ticket</label>
                 </div>
                 <form method="POST" @submit.prevent="onSubmit">
                     <div class="row q-col-gutter-lg">
-                        <div class="col-12 col-md-6">
+                        <div class="col-12">
                             <label class="text-primary">Details</label>
                             <q-input type="text" outlined class="q-mb-sm bg-grey-1" v-model="form.title"
                                 label="Title" />
                             <q-input type="textarea" outlined class="q-mb-sm bg-grey-1" v-model="form.description"
                                 label="Description" />
                         </div>
-                        <div class="col-12 col-md-6">
+                    </div>
+                    <div class="row q-col-gutter-md">
+                        <div class="col-12">
+                            <label class="text-primary">Images</label>
+                            <q-uploader style="width: 100%" class="q-mb-md" :max-files="10" :max-file-size="5000000"
+                                hide-upload-btn url="http://localhost:4444/upload" multiple accept=".jpg, image/*"
+                                ref="imageUploader" @uploaded="imageUploaded" @uploading="imageUploading"
+                                @failed="imageUploadFailed" />
+                        </div>
+                    </div>
+                    <div class="row q-col-gutter-md">
+                        <div class="col-12">
                             <label class="text-primary">Address</label>
                             <div class="row q-col-gutter-md">
                                 <div class="col-12">
