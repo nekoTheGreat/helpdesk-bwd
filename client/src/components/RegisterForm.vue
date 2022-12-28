a60c5bc090c7a59f65039828cb432602e9aaabde
Kevin Orge <kevinorge@gmail.com>
2022-12-28 13:56:14 +0800

    updated register for

--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -15,6 +15,7 @@ const form = reactive({
     city: '',
     munipality: '',
     province: '',
+    contact_number: '',
 });
 const onSubmit = () => {
     console.log(Object.assign({}, form));
@@ -26,11 +27,14 @@ const onCancel = () => {
 <template>
     <q-page class="q-pa-sm">
         <div class="row justify-center">
-            <div class="col-12 col-sm-10">
+            <div class="col-12 col-sm-11 col-md-9 col-lg-7 col-xl-5">
                 <div class="q-px-lg q-mt-lg q-pt-sm q-pb-lg bg-primary">
                     <form method="POST" @submit.prevent="onSubmit">
                         <div class="q-mb-lg">
                             <label class="text-h6 text-weight-bold text-secondary">Register</label>
+                        </div>
+                        <div class="q-mb-sm">
+                            <label class="text-secondary">Credentials</label>
                         </div>
                         <div class="row q-col-gutter-md">
                             <div class="col-12 col-sm-4">
@@ -46,6 +50,9 @@ const onCancel = () => {
                                     v-model="form.confirm_password" label="Confirm Password" />
                             </div>
                         </div>
+                        <div class="q-mb-sm">
+                            <label class="text-secondary">User Details</label>
+                        </div>
                         <div class="row q-col-gutter-md">
                             <div class="col-12 col-sm-4">
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.first_name"
@@ -59,8 +66,15 @@ const onCancel = () => {
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.last_name"
                                     label="Last Name" />
                             </div>
+                            <div class="col-12 col-sm-4">
+                                <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.contact_number"
+                                    label="Contact Number" />
+                            </div>
                         </div>
-                        <div class="row q-col-gutter-md">
+                        <div class="q-mb-sm">
+                            <label class="text-secondary">Address</label>
+                        </div>
+                        <div class="row q-col-gutter-md q-mb-md">
                             <div class="col-12 col-sm-8">
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.address"
                                     label="Address" />
@@ -68,8 +82,6 @@ const onCancel = () => {
                             <div class="col-12 col-sm-4">
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.purok" label="Purok" />
                             </div>
-                        </div>
-                        <div class="row q-col-gutter-md">
                             <div class="col-12 col-sm-4">
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.barangay"
                                     label="Barangay" />
@@ -81,8 +93,6 @@ const onCancel = () => {
                             <div class="col-12 col-sm-4">
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.city" label="City" />
                             </div>
-                        </div>
-                        <div class="row q-col-gutter-md">
                             <div class="col-12 col-sm-4">
                                 <q-input outlined class="col-4 q-mb-sm bg-grey-1" v-model="form.province"
                                     label="Province" />
@@ -90,9 +100,10 @@ const onCancel = () => {
                         </div>
                         <div class="row q-col-gutter-md">
                             <div class="col-12 text-right">
-                                <q-btn type="submit" color="secondary" text-color="primary" class="q-mr-md"
-                                    label="Save" />
-                                <q-btn color="white" text-color="primary" label="Cancel" @click="onCancel" />
+                                <q-btn type="submit" color="secondary" text-color="primary" class="q-mr-md" label="Save"
+                                    style="max-width: 70px;" />
+                                <q-btn color="white" text-color="primary" label="Cancel" @click="onCancel"
+                                    style="max-width: 70px;" />
                             </div>
                         </div>
                     </form>
