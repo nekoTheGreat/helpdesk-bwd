ba8d2f6e481b88499a9558eaca281780eb5b7c56
Kevin Orge <kevinorge@gmail.com>
2023-01-02 23:58:32 +0800

    replace authenticated to token in auth store; depends on remember me flag, use session or local storage

--- client/src/stores/auth.ts
+++ client/src/stores/auth.ts
@@ -1,9 +1,11 @@
 import { defineStore } from 'pinia';
-import { useLocalStorage } from '@vueuse/core';
+import { useLocalStorage, useSessionStorage } from '@vueuse/core';
+
+const TOKEN_STORAGE_KEY = 'bwd.heldesk.token';
 
 export const useAuth = defineStore('auth', {
   state: () => ({
-    authenticated: useLocalStorage('bwd.heldesk.authenticated', false),
+    token: useLocalStorage(TOKEN_STORAGE_KEY, ''),
     user: useLocalStorage('bwd.heldesk.user', {
       email: '',
       first_name: '',
@@ -13,26 +15,33 @@ export const useAuth = defineStore('auth', {
 
   getters: {
     isAuthenticated(state){
-      return state.authenticated;
-    },
-    userName(state){
-      return `${state.user.last_name}, ${state.user.first_name}`;
+      return state.token != '';
     },
     userEmail(state){
       return state.user.email;
     },
+    getToken(state){
+      return state.token;
+    }
   },
 
   actions: {
-    loggedIn(user: any) {
-      this.user.email = user.email;
-      this.user.first_name = user.first_name;
-      this.user.last_name = user.last_name;
-      this.authenticated = true;
+    loggedIn(data: any, rememberMe: boolean) {
+      this.user.email = data.email;
+      this.user.first_name = data.first_name;
+      this.user.last_name = data.last_name;
+      this.token = data.token;
+      if(rememberMe){
+        useLocalStorage(TOKEN_STORAGE_KEY, data.token);
+        sessionStorage.removeItem(TOKEN_STORAGE_KEY);
+      }else{
+        localStorage.removeItem(TOKEN_STORAGE_KEY);
+        useSessionStorage(TOKEN_STORAGE_KEY, data.token);
+      }
     },
     loggedOut(){
       this.user.email = this.user.first_name = this.user.last_name = '';
-      this.authenticated = false;
+      this.token = '';
     }
   }
 });
