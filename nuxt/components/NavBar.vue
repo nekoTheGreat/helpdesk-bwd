ec4e95010619e60d4b6d853147966c6783155f11
Kevin Orge <kevinorge@gmail.com>
2023-02-12 22:08:11 +0800

    nuxt - setup pages and links in navbar

--- nuxt/components/NavBar.vue
+++ nuxt/components/NavBar.vue
@@ -3,6 +3,10 @@ import { Collapse } from 'bootstrap/dist/js/bootstrap.esm.js';
 import { useAuthStore } from '~~/stores/authStore';
 
 const authStore = useAuthStore();
+const route = useRoute();
+const isLinkActive = (link: string) => {
+    return route.path == link;
+};
 onMounted(() => {
     new Collapse('#mainNavBarDropdown');
 });
@@ -11,8 +15,9 @@ onMounted(() => {
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
         <div class="container-fluid">
             <div class="me-1" style="position:relative; width:50px;height:50px;">
-                <a class="navbar-brand" style="position:absolute; top: -5px;width:100%" href="/"><img
-                        src="~/assets/bwd.png" style="width:50px;" /></a>
+                <NuxtLink class="navbar-brand" style="position:absolute; top: -5px;width:100%" to="/">
+                    <img src="~/assets/bwd.png" style="width:50px;" />
+                </NuxtLink>
             </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavBarDropdown"
                 aria-controls="mainNavBarDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,29 +26,44 @@ onMounted(() => {
             <div class="collapse navbar-collapse" id="mainNavBarDropdown">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Tickets</a>
+                        <NuxtLink class="nav-link" :class="{ 'active': isLinkActive('/tickets') }" to="/tickets">
+                            Tickets</NuxtLink>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <template v-if="!authStore.authenticated">
                         <li>
-                            <a class="nav-link">Login</a>
+                            <NuxtLink class="nav-link" :class="{ 'active': isLinkActive('/login') }" to="/login">
+                                Login
+                            </NuxtLink>
                         </li>
                         <li>
-                            <a class="nav-link">Signup</a>
+                            <NuxtLink class="nav-link" :class="{ 'active': isLinkActive('/signup') }" to="/signup">
+                                Signup
+                            </NuxtLink>
                         </li>
                     </template>
                     <li class="nav-item dropdown" v-else>
-                        <a class="nav-link dropdown-toggle" href="#" id="mainNavBarAccountDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" :class="{ 'active': isLinkActive('/profile') }" href="#"
+                            id="mainNavBarAccountDropdown" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
                             Account
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="mainNavBarAccountDropdown">
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <NuxtLink class="dropdown-item" :class="{ 'active': isLinkActive('/profile') }"
+                                    to="/profile">
+                                    Profile
+                                </NuxtLink>
+                            </li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                            <li>
+                                <NuxtLink class="dropdown-item" to="/logout">
+                                    Logout
+                                </NuxtLink>
+                            </li>
                         </ul>
                     </li>
                 </ul>
---
+++ nuxt/pages/login.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="container">
+        Login
+    </div>
+</template>
