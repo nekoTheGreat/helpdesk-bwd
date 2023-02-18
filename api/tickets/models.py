3a1f0c1366024fd3a6859fe77b0e0a7c824f98a8
Kevin Orge <kevinorge@gmail.com>
2023-02-18 23:34:51 +0800

    api - removed poblacion from ticket model

--- api/tickets/models.py
+++ api/tickets/models.py
@@ -6,7 +6,6 @@ class Ticket(BaseModel):
     description = models.CharField(max_length=500)
     street_address = models.CharField(max_length=255, null=True, blank=True)
     purok = models.CharField(max_length=255, null=True, blank=True)
-    poblacion = models.CharField(max_length=255, null=True, blank=True)
     barangay = models.CharField(max_length=255, null=True, blank=True)
     municipality = models.CharField(max_length=255, null=True, blank=True)
     user = models.ForeignKey("neko_users.User", db_column="user_id", on_delete=models.CASCADE, default=None)
