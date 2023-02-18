7ac8c7239ddfa4df8e6d6403993888b22d529737
Kevin Orge <kevinorge@gmail.com>
2023-02-18 23:31:56 +0800

    nuxt - base template of update and create ticket form page

---
+++ nuxt/pages/tickets/[id].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute();
+const id = route.params.id * 1;
+</script>
+<template>
+    <div class="container">
+        <TicketView :id="id"></TicketView>
+    </div>
+</template>
