b5a8bcbb048984d7208df8266c3eac69d6e66516
Kevin Orge <kevinorge@gmail.com>
2023-02-18 23:37:38 +0800

    nuxt - added interface types of ticket and attachment

--- nuxt/types/api.ts
+++ nuxt/types/api.ts
@@ -5,3 +5,25 @@ export interface ApiError{
     errors?: Dict,
     status: number,
 }
+export interface Attachment{
+    id: number,
+    type: string,
+    identifier: number,
+    disk_type: string,
+    file_path: string,
+    is_active: number,
+    file_name: string,
+    unique_file_name: string,
+    url: string,
+}
+export interface Ticket{
+    id: number,
+    subject: string,
+    description: string,
+    street_address: string,
+    purok: string,
+    barangay: string,
+    municipality: string,
+    user: number,
+    photos: Attachment[],
+}
