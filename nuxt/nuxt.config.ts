227b4574cfbfb9e61d7be68805aa2f262847f4a5
Kevin Orge <kevinorge@gmail.com>
2023-02-18 14:34:18 +0800

    nuxt - set public config

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,16 @@
+import { processExpression } from "@vue/compiler-core";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
         '@pinia/nuxt',
     ],
+    runtimeConfig:{
+        api_url: process.env.API_URL,
+        public: {
+            api_url: process.env.API_URL,
+        }
+    },
+    debug: process.env.DEBUG,
+    ssr: false,
 })
