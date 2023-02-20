cb6240bd3e47830d0210ca3308f5d937b7ec53e3
Kevin Orge <kevinorge@gmail.com>
2023-02-20 13:30:24 +0800

    nuxt - added bansalan barangays and municipality options

--- nuxt/stores/options.ts
+++ nuxt/stores/options.ts
@@ -1,3 +1,13 @@
 export const subjectOptions = [
     'Bills', 'General', 'No Water', 'Water Disconnection',
 ];
+export const barangayOptions = {
+    'Bansalan' : [
+        'Alegre','Alta Vista','Anonang','Bitaug','Bonifacio','Buenavista','Darapuay','Dolo','Eman',
+        'Kinuskusan','Libertad','Linawan','Mabuhay','Mabunga','Managa','Marber','New Clarin',
+        'Poblacion Uno','Poblacion Dos','Rizal','Santo Niño','Sibayan','Tinongtongan','Tubod','Union'
+    ],
+};
+export const municipalityOptions = [
+    'Bansalan', 'Magsaysay', 'Matanano',
+];
