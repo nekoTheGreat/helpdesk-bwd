f31fbeac3c140dc65fe221a55172e6d2a373ac41
Kevin Orge <kevinorge@gmail.com>
2023-02-20 13:58:13 +0800

    nuxt - base template of handling new/edited ticket data from ticket form

--- nuxt/pages/tickets/new.vue
+++ nuxt/pages/tickets/new.vue
@@ -1,6 +1,11 @@
-<script setup></script>
+<script setup>
+
+const onSubmit = (form) => {
+    console.log(form);
+};
+</script>
 <template>
     <div class="container">
-        <TicketForm></TicketForm>
+        <TicketForm @update:model-value="onSubmit"></TicketForm>
     </div>
 </template>
