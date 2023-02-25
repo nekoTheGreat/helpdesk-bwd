48962e627ce9bcfbaf0c839c989915dc6276c636
Kevin Orge <kevinorge@gmail.com>
2023-02-25 21:50:06 +0800

    nuxt - file input component's multiple model where it adds deletes flag

--- nuxt/components/inputs/FileInput.vue
+++ nuxt/components/inputs/FileInput.vue
@@ -1,36 +1,78 @@
 <script setup lang="ts">
-import { Attachment } from '~~/types/api';
+import { AttachmentForm } from '~~/types/api';
 
 interface Props {
-    attachments?: Attachment[],
+    attachments?: AttachmentForm[],
 }
 const attrs = useAttrs();
 const props = defineProps<Props>();
-const emit = defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue', 'update:attachments']);
 const uploaded_files = ref([]);
+const attachments = ref<AttachmentForm[]>([]);
+
 const files = computed(() => {
-    let items = props.attachments ?? [];
+    let items = Object.assign([], attachments.value);
     uploaded_files.value.forEach((it) => {
         it.file_name = it.name;
         items.push(it);
     });
     return items;
 });
-const onInput = (evt) => {
+
+const onInput = (evt: Event) => {
     uploaded_files.value = [];
     if (evt.target.files.length == 0) return;
     for (const file of evt.target.files) {
         file.file_name = file.name;
         uploaded_files.value.push(file);
     }
-
-    emit('update:modelValue', files);
+    emit('update:modelValue', uploaded_files.value);
 };
+const onToggleFile = (file: any) => {
+    const index = files.value.findIndex(it => it == file);
+    const found = index > -1 ? files.value[index] : null;
+    if (found == null) return;
+    if (found.id) {
+        attachments.value[index].is_deleted = !attachments.value[index].is_deleted;
+        emit('update:attachments', Object.assign([], attachments.value));
+    } else {
+        const index = uploaded_files.value.findIndex(it => it == file);
+        if (index > -1) uploaded_files.value.splice(index, 1);
+    }
+};
+const fileInRemoved = (file: AttachmentForm) => {
+    const index = attachments.value.findIndex(it => it.id == file.id);
+    if (index > -1) return attachments.value[index].is_deleted;
+    return false;
+}
+const fileClasses = (file: AttachmentForm) => {
+    if (file.id == undefined) return '';
+    let classes = [];
+    if (fileInRemoved(file)) {
+        classes.push('text-decoration-line-through');
+    }
+    return classes.join(' ');
+};
+
+onMounted(() => {
+    if (props.attachments) {
+        props.attachments.forEach(it => {
+            it.is_deleted = false;
+            attachments.value.push(it);
+        });
+    }
+});
 </script>
 <template>
     <div>
         <div v-if="files.length">
-            <span v-for="file in files" class="badge bg-info me-1 mb-2 text-dark">{{ file.file_name }}</span>
+            <span v-for="file in files" class="badge bg-info me-1 mb-2 text-dark fs-6" :class="fileClasses(file)">
+                {{ file.file_name }}
+                <a v-if="!fileInRemoved(file)" @click.prevent="onToggleFile(file)" href="#" class="ms-1 text-dark"><i
+                        class="bi bi-trash3"></i></a>
+                <a v-else @click.prevent="onToggleFile(file)" href="#" class="ms-1 text-dark"><i
+                        class="bi bi-arrow-counterclockwise"></i></a>
+            </span>
         </div>
         <input v-bind="attrs" class="form-control" type="file" @input="onInput">
     </div>
