336d8ac8374d80bfc1b18442d7ac86b7e063ed27
Kevin Orge <kevinorge@gmail.com>
2023-02-26 14:08:14 +0800

    nuxt - remove async from composable

--- nuxt/composables/findOrFailTicket.ts
+++ nuxt/composables/findOrFailTicket.ts
@@ -1,6 +1,6 @@
 import TicketService from '~~/services/TicketService';
 
-export default async function useFindOrFailTicket(){
+export default function useFindOrFailTicket(){
     const route = useRoute();
     const id = route.params.id * 1;
     const ticketService = new TicketService();
