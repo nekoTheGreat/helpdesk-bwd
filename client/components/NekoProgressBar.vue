2a1fbf2b4e018d90f260558edc1254f34c132893
Kevin Orge <kevinorge@gmail.com>
2023-03-11 20:55:21 +0800

    client - progress bar component

---
+++ client/components/NekoProgressBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="progress rounded-0" style="height: 10px;">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Loading..."
+            style="width: 100%;"></div>
+    </div>
+</template>
