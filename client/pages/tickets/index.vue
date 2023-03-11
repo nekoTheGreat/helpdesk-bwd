4da5381528c18790ca191624fc41e14f7db41303
Kevin Orge <kevinorge@gmail.com>
2023-03-11 17:07:45 +0800

    client - use data table component in tickets page

--- client/pages/tickets/index.vue
+++ client/pages/tickets/index.vue
@@ -4,11 +4,21 @@ import TicketService from '~~/services/TicketService';
 import { Ticket } from '~~/types/api';
 import { fullAddressFromTicket } from '~~/utils/misc';
 import { useAuthStore } from '~~/stores/authStore';
+import { NekoDataTableColumn } from '~~/types/neko-components';
 
 const authStore = useAuthStore();
 const tickets = ref<Ticket[]>([]);
 const ticketService = new TicketService();
 const destroyTicket = useDestroyTicket();
+let datatable_columns = [
+    { name: 'id', label: 'Id', slot: true },
+    { name: 'subject', label: 'Subject' },
+    { name: 'description', label: 'Description' },
+    { name: 'address', label: 'Address', slot: true },
+] as NekoDataTableColumn[];
+if (authStore.authenticated) {
+    datatable_columns.push({ name: 'actions', label: 'Actions', slot: true, class: 'text-center', style: 'width: 70px;' });
+}
 
 const onDeleteTicket = async (ticket: Ticket) => {
     const res = await destroyTicket.destroy(ticket, { disableRedirect: true });
@@ -28,34 +38,18 @@ onMounted(async () => {
         <div class="d-flex justify-content-end mb-2">
             <NuxtLink v-if="authStore.authenticated" class="btn btn-primary" to="/tickets/new">Create Ticket</NuxtLink>
         </div>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Subject</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-if="tickets.length == 0">
-                    <td colspan="5" class="text-center">No records found</td>
-                </tr>
-                <tr v-for="ticket in tickets" :key="ticket.id">
-                    <td scope="row">
-                        <NuxtLink :to="'/tickets/' + ticket.id">{{ ticket.id }}</NuxtLink>
-                    </td>
-                    <td>{{ ticket.subject }}</td>
-                    <td>{{ ticket.description }}</td>
-                    <td>{{ fullAddressFromTicket(ticket) }}</td>
-                    <td>
-                        <a href="#" @click.prevent="onDeleteTicket(ticket)" class="me-2" alt="Delete Ticket">
-                            <i class="bi bi-trash3"></i>
-                        </a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <NekoDataTable :columns="datatable_columns" :items="tickets">
+            <template #id="slotProps">
+                <NuxtLink :to="'/tickets/' + slotProps.data.id">{{ slotProps.data.id }}</NuxtLink>
+            </template>
+            <template #address="slotProps">
+                {{ fullAddressFromTicket(slotProps.data) }}
+            </template>
+            <template #actions="slotProps">
+                <a href="#" @click.prevent="onDeleteTicket(slotProps.data)" class="me-2" alt="Delete Ticket">
+                    <i class="bi bi-trash3"></i>
+                </a>
+            </template>
+        </NekoDataTable>
     </div>
 </template>
