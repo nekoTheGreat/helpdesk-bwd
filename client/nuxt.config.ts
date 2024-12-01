e8e5717c305c35373387938d115dd4cfedadd9cd
Kevin Orge <kevinorge@gmail.com>
2024-12-01 09:54:32 +0800

    client - setup proxy for api resource

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -5,10 +5,20 @@ export default defineNuxtConfig({
     modules: [
         '@pinia/nuxt',
     ],
+    axios: {
+        proxy: true,
+    },
+    proxy: {
+        "/api": {
+            target: process.env.API_URL,
+            pathRewrite: { "^/api": "/api" },
+            changeOrigin: true,
+        }
+    },
     runtimeConfig:{
         api_url: process.env.API_URL,
         public: {
-            api_url: process.env.API_URL,
+            api_url: "/api/",
             recaptcha_key: process.env.reCaptchaKey,
         }
     },
